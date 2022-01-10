7e0e19fe72a6e71217cb571ad69e225af543fd8d
Emily Chen <emchen.c@gmail.com>
2022-01-10 09:24:44 +0000

    deleted unnecessary const

--- static/js/review.js
+++ static/js/review.js
@@ -1,10 +1,6 @@
 'use strict';
 
-const button = document.querySelector('#leave-review');
-
-// document.querySelector('#file').addEventListener('submit', event())
-
-button.addEventListener('submit', evt => {
+document.querySelector('#leave-review').addEventListener('submit', evt => {
     evt.preventDefault();
 
     const form = new FormData(); 
@@ -17,25 +13,12 @@ button.addEventListener('submit', evt => {
     form.append('user', document.querySelector('#hidden-user').value);
     form.append('img', document.querySelector('#file').files[0]);
 
-    // const allDetails = {
-    //     name: document.querySelector('#store_name').innerText,
-    //     address: document.querySelector('#store_address').innerText,
-    //     zip: document.querySelector('#store_zip').innerText,
-    //     phone: document.querySelector('#store_phone').innerText,
-    //     id: location.pathname.split('/')[2],
-    //     review: document.querySelector('#reviewed').value,
-    //     user: document.querySelector('#hidden-user').value
-    // };
-
-
-    console.log(form)
     fetch('/new-shop', {
         method: 'POST',
         body: form
     })
       .then(response => response.text()) 
       .then(responseData => { 
-          console.log(responseData)
           if (responseData == 'Review created') {
               alert('Thanks for your review!');
               document.querySelector('ul').insertAdjacentHTML('afterbegin', `<li> ${form.review} </li>`);
